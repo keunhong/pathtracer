6e4addb6e33816a938c41de44e51111d4bfe02b2
Keunhong Park <k@void.im>
2012-03-14 15:49:51 -0500

    Initial Commit

---
+++ pathtracer/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  pathtracer
+//
+//  Created by Keunhong Park on 3/14/12.
+//  Copyright (c) 2012 UIUC. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
